d9bc9d72cfc6d6df2d5f46414d6e51c618041b83
Ryan Richard <ryanrichard07@gmail.com>
2016-04-19 10:18:19 -0500

    Move Account Struct to SDK from CLI
    * Previously the Account type was part of the CLI. It's now part of the
    SDK and the various methods that used take arguments now act on an
    Account.

--- service/authorization/authorization.go
+++ service/authorization/authorization.go
@@ -1,6 +1,24 @@
+/*
+Copyright 2016 Kumoru.io
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+//Package authorization provides an Account type and pertinent methods for this type.
 package authorization
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -8,6 +26,86 @@ import (
 	"github.com/kumoru/kumoru-sdk-go/kumoru"
 )
 
+//Account represents a user and pretinent metadata about that user.
+type Account struct {
+	CreatedAt string `json:"created_at"`
+	Email     string `json:"email"`
+	GivenName string `json:"given_name"`
+	Surname   string `json:"surname"`
+	UpdatedAt string `json:"updated_at"`
+}
+
+//CreateAcct requests a particular account be made in Kumoru.
+//It returns the updated Account.
+func (a *Account) CreateAcct(password string) (*Account, *http.Response, []error) {
+	k := kumoru.New()
+
+	k.Put(fmt.Sprintf("%s/v1/accounts/%s", k.EndPoint.Authorization, a.Email))
+	k.Send(fmt.Sprintf("given_name=%s&surname=%s&password=%s", a.GivenName, a.Surname, password))
+
+	resp, body, errs := k.End()
+
+	if len(errs) > 0 {
+		return a, resp, errs
+	}
+
+	if resp.StatusCode >= 400 {
+		errs = append(errs, fmt.Errorf("%s", resp.Status))
+	}
+
+	err := json.Unmarshal([]byte(body), &a)
+
+	if err != nil {
+		errs = append(errs, err)
+		return a, resp, errs
+	}
+
+	return a, resp, nil
+}
+
+//ResetPassword requests the password be reset for a given Account.
+func (a *Account) ResetPassword() (*Account, *http.Response, []error) {
+	k := kumoru.New()
+
+	k.Get(fmt.Sprintf("%v/v1/accounts/%v/password/resets/", k.EndPoint.Authorization, a.Email))
+	resp, _, errs := k.End()
+
+	if errs != nil {
+		return a, resp, errs
+	}
+
+	return a, resp, nil
+}
+
+//ShowAcct requests account details from Kumoru and marshals the data into the Account type.
+func (a *Account) ShowAcct() (*Account, *http.Response, []error) {
+	k := kumoru.New()
+
+	k.Get(fmt.Sprintf("%v/v1/accounts/%v", k.EndPoint.Authorization, a.Email))
+	k.SignRequest(true)
+
+	resp, body, errs := k.End()
+
+	if len(errs) > 0 {
+		return a, resp, errs
+	}
+
+	if resp.StatusCode >= 400 {
+		errs = append(errs, fmt.Errorf("%s", resp.Status))
+		return a, resp, errs
+	}
+
+	err := json.Unmarshal([]byte(body), &a)
+
+	if err != nil {
+		errs = append(errs, err)
+		return a, resp, errs
+	}
+
+	return a, resp, nil
+}
+
+//GetTokens generates a new token(uuid), stores this token in Kumoru and retrieves the private half of the token.
 func GetTokens(username, password string) (string, *http.Response, string, []error) {
 	k := kumoru.New()
 
@@ -18,27 +116,4 @@ func GetTokens(username, password string) (string, *http.Response, string, []err
 	resp, body, errs := k.End()
 
 	return token, resp, body, errs
-}
-
-func CreateAcct(username, given_name, surname, password string) (*http.Response, string, []error) {
-	k := kumoru.New()
-
-	k.Put(fmt.Sprintf("%s/v1/accounts/%s", k.EndPoint.Authorization, username))
-	k.Send(fmt.Sprintf("given_name=%s&surname=%s&password=%s", given_name, surname, password))
-	return k.End()
-}
-
-func ResetPassword(email string) (*http.Response, string, []error) {
-	k := kumoru.New()
-
-	k.Get(fmt.Sprintf("%v/v1/accounts/%v/password/resets/", k.EndPoint.Authorization, email))
-	return k.End()
-}
-
-func ShowAcct(username string) (*http.Response, string, []error) {
-	k := kumoru.New()
-
-	k.Get(fmt.Sprintf("%v/v1/accounts/%v", k.EndPoint.Authorization, username))
-	k.SignRequest(true)
-	return k.End()
 }
